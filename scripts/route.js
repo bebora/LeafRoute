aad9131033a58a942333969710a27ccec44b3084
Simone <simone.rizzo@live.com>
2019-03-26 23:13:37 +0100

    Add search box over map

---
+++ scripts/route.js
@@ -0,0 +1,37 @@
+$(document).ready(function()
+{
+    var map = L.map('map').setView([45.465756, 9.187551], 14);
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    function onLocationFound(e) {
+        var radius = e.accuracy / 2;
+        L.marker(e.latlng).addTo(map)
+            .bindPopup("You are within " + radius + " meters from this point").openPopup();
+        L.circle(e.latlng, radius).addTo(map);
+    }
+    //TODO remove location if useless
+    map.on('locationfound', onLocationFound);
+
+    function onLocationError(e) {
+        console.log(e.message);
+    }
+
+    map.on('locationerror', onLocationError);
+
+    map.locate({setView: true, maxZoom: 16});
+
+    polyline = L.polyline([]).addTo(map);
+    function showpoly(){
+        var lat = document.getElementById('lat').value;
+        var lng = document.getElementById('lng').value;
+        console.log(lat);
+        console.log(lng);
+        polyline.addLatLng(L.latLng(parseInt(lat,10),parseInt(lng,10)));
+        map.fitBounds(polyline.getBounds());
+      }
+    map.addLayer(polyline);
+    document.getElementById("button").addEventListener("click", showpoly);
+})
