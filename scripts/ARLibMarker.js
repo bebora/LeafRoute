49ae3239b490fc5bc8f038f01a7bf3b80a7faca5
klaa97 <10521251@polimi.it>
2019-04-27 19:36:53 +0200

    Make ARLibMarker compatible with LeafLet guidelines

--- scripts/ARLibMarker.js
+++ scripts/ARLibMarker.js
@@ -1,11 +1,11 @@
-class ARLibMarker {
+L.Marker.MovingMarker.ARLibMarker = L.Marker.MovingMarker.extend({
     /**
      * Construct an ARLibMarker, a wrapper of a MovingMarker, given the waypoints and set it ready when it's ready to be added to the LeafLet map
      * @param {Array} startPoint array with latitude and longitude of the starting point
      * @param {*} destination array with the latitude and longitude of destination point
      * @param {*} option optional parameters defining the speed, the timing of rerouting and the endpoint used as a routing machine
      */
-    constructor(startPoint, destination, rerouting = true, speed =100, timer = 1000, endpoint = 'http://localhost:1337/getroutes?') {
+    initialize: function (startPoint, destination, rerouting = true, speed =100, timer = 1000, endpoint = 'http://localhost:1337/getroutes?') {
         var temp = [startPoint]
         this.speed = speed;
         this.timer = timer;
@@ -24,14 +24,14 @@ class ARLibMarker {
     }).fail(function(textStatus, error) {
             console.log("Request Failed: " + textStatus + ", " + error);
         });       
-    }
+    },
     /**
      * Util function to construct the full route and the related Queues
      * @param {} that scope of the ARLibMarker class
      * @param {Array} latlngs list of coordinates used for the route
      */
-    _buildPath(that, latlngs) {
-        that.marker = new L.Marker.MovingMarker([latlngs[0]], 0);
+    _buildPath: function(that, latlngs) {
+        L.Marker.MovingMarker.prototype.initialize.call(that,[latlngs[0]], 0);
         that.destination = latlngs[latlngs.length-1];
         if (latlngs.length >= 4) {
             that.QueueLatlngs = latlngs.slice(3);
@@ -42,7 +42,7 @@ class ARLibMarker {
             for (var j=0; j < 3; j++) {
                 var length = L.latLng(that.currentLatlngs[j]).distanceTo(L.latLng(that.currentLatlngs[j+1]));
                 var time_seconds = (length / (that.speed / 3.6));
-                that.marker.addLatLng(that.currentLatlngs[j+1], time_seconds*1000);
+                L.Marker.MovingMarker.prototype.addLatLng.call(that, that.currentLatlngs[j+1], time_seconds*1000);
             }
             that.rerouting = that._rerouting;
             
@@ -50,20 +50,20 @@ class ARLibMarker {
             for (var j=0; j < latlngs.length; j++) {
                 var length = L.latLng(latlngs[j]).distanceTo(L.latLng(latlngs[j+1]));
                 var time_seconds = (length / (that.speed / 3.6));
-                that.marker.addLatLng(latlngs[j+1], time_seconds*1000);
+                L.Marker.MovingMarker.prototype.addLatLng.call(that, latlngs[j+1], time_seconds*1000);
             }
         }
         else {
             that.rerouting = false;
             that.reroute = false;
         }
-    }
+    },
 
     /**
      * Util function for rerouting machine, fetching the endpoint for different paths from original
      * @param {} that original scope
      */
-    _fetchroute(that) { 
+    _fetchroute: function(that) { 
         var current_index = that.current_index;
         if (that.reroute && that.rerouting && that.QueueLatlngs.length > 1 && that.currentLatlngs.length > current_index + 2) {
             var startPoint = that.currentLatlngs[that.current_index + 2];
@@ -83,30 +83,30 @@ class ARLibMarker {
         else {
             console.log("No rerouting!");
         }
-    }
+    },
 
     /**
      * Add the marker in the map, autostarting the route
      * @param {L.map} map map used to visualize the MovingMarker
      */
-    addTo(map) {
+    addTo: function(map) {
         var that = this;
         if (!this.ready) {
             setTimeout(function(){that.addTo(map)},100);
         } else {
             that._startRoute();
-            that.marker.addTo(map);
+            L.Marker.MovingMarker.prototype.addTo.call(that, map);
         }
         that.map = map;
-    }
+    },
 
     /**
      * Set up events triggered function and start the marker
      */
-    _startRoute() {
+    _startRoute: function() {
     var that = this;
     if (that.rerouting) {
-        that.marker.on('checkpoint',function() {
+        that.on('checkpoint',function() {
             console.log(that.current_index);
             if (that.QueueLatlngs.length > 1) {
                 that.QueueLatlngs = that.tempQueueLatlngs.slice(1);
@@ -115,18 +115,20 @@ class ARLibMarker {
                 var oldlast = L.latLng(that.currentLatlngs[that.currentLatlngs.length-2]);
                 var newlast = L.latLng(that.currentLatlngs[that.currentLatlngs.length-1]);
                 var duration = oldlast.distanceTo(newlast) / (that.speed / 3.6);
-                that.marker.addLatLng(that.QueueLatlngs[0], duration * 1000);   
+                L.Marker.MovingMarker.prototype.addLatLng.call(that,that.QueueLatlngs[0], duration * 1000);   
                 that.current_index = that.current_index + 1;
             }   
         });
         that.interval = window.setInterval(that._fetchroute(that) , that.timer);
     }
-    that.marker.on('end', function() {
-        that.map.removeLayer(that.marker);
+    that.on('end', function() {
+        that.map.removeLayer(that);
         window.clearInterval(that.interval);
         that.rerouting = false;
     });
-    that.marker.start();
-    }
-};
+    L.Marker.MovingMarker.prototype.start.call(that);    
+}
+});
+
+
 
