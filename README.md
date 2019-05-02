a296879a3eb308dd9b29b40605d774029b95be76
bebora <simone3.rizzo@mail.polimi.it>
2019-05-03 01:25:17 +0200

    Add sliders to simulation page

--- README.md
+++ README.md
@@ -9,7 +9,9 @@ Plugins:
 Backend built with Boost.Graph library [arlib](https://github.com/leonardoarcari/arlib)  
 Demo [here](https://bebora.github.io/LeafRoute/route.html). Backend must be running at localhost.  
 Milan zones from [Comune di Milano](https://geoportale.comune.milano.it/ATOM/SIT/Municipi/Municipi_Dataset_1.xml)
+Sliders with constant sum inspired from [constant-sum-sliders](https://github.com/jacobsolomon15/constant-sum-sliders)  
 # TODO
 - Add bootstrap sidebar to setup and start the simulation
 - Add function for random generation of points inside a polygon (using turf.js)
 - Add documentation for RoutesDealer
+- Join some custom css files
---
+++ scripts/autosliders.js
@@ -0,0 +1,95 @@
+$('.slider-auto-reallocate-input').each(function() {
+    $(this).on('input', function () {
+        var $this = $(this);
+        $this.trigger('change'); // A workaround to make the slider update with each move, before the cursor is released
+        
+        var total_sliders = $('.slider-auto-reallocate-input').length;
+        
+        if ($this.prop('disabled') == true) {
+            return true;
+        }
+        
+        var val = $this.val();
+        var val_pct = (val - $this.attr("min")) / ($this.attr('max') - $this.attr('min')) * 100;
+        
+        if (val_pct < 0) {
+            val_pct = 0;
+        }
+        val_pct++;
+        // Adjust the css so the bar looks like it grows and shrinks
+        var st = 'linear-gradient(to right, rgb(35, 175, 0) ' + val_pct + '%, white ' + val_pct + '%)';
+        $this.css('background', st);
+
+        var total = 0;
+        
+        $('.slider-auto-reallocate-input').not($this).not('.locked').each(function () {
+        
+           total += parseInt($(this).val());
+        });
+        
+        total += parseInt($this.val());
+        
+        var availableTotal = 900;
+        
+        var delta = availableTotal - total;
+        
+        
+        $('.slider-auto-reallocate-input').not($this).each(function () {
+           var value = parseInt($(this).val());
+           var new_val = value + (delta/(total_sliders -1));
+           
+           var val_pct = (new_val - $this.attr("min")) / ($this.attr('max') - $this.attr('min')) * 100;
+           
+           if (val_pct < 0) {
+            val_pct = 0;
+           }
+           val_pct++;
+           var st = 'linear-gradient(to right, rgb(35, 175, 0) ' + val_pct + '%, white ' + val_pct + '%)';
+           $(this).css('background', st);
+
+           if (new_val < 0 || $this.val() == 100) {
+            new_val = 0;
+           }
+           if (new_val > 100) {
+            new_val = 100;
+           }
+           
+           $(this).val(new_val);
+        });
+    });
+});
+
+function equalize(group) {
+    var options = $(group).parent().find('.option');
+    var availableTotal = 900;
+    var portion = availableTotal / options.length;
+    $(options).each(function () {
+        $(this).val(portion);
+        var val_pct = (portion / availableTotal)*100;
+        if (val_pct < 0) {
+         val_pct = 0;
+        }
+        if (val_pct<60){
+            val_pct++;
+        }
+        var st = 'linear-gradient(to right, rgb(35, 175, 0) ' + val_pct + '%, white ' + val_pct + '%)';
+        $(this).css('background', st);
+    });
+}
+//equalize on start to prevent visual issues on refresh
+equalize($("#reset"));
+$("#totalmarkers").val(400);
+$("#totalmarkers").css('background', 'linear-gradient(to right, rgb(35, 175, 0) 40%, white 40%)');
+
+//fill background of sliders
+$('.pretty-slider').on('input', function() {
+    var fill_percent = ($(this).val() - $(this).attr("min"))/($(this).attr("max") - $(this).attr("min"));
+    var discrete_percent = fill_percent.toFixed(2)*100;
+    var st = 'linear-gradient(to right, rgb(35, 175, 0) ' + discrete_percent + '%, white ' + discrete_percent + '%)';
+    console.log(st);
+    $(this).css('background', st);
+});
+
+
+
+
