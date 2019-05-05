917578436593abd6fb0ae6e36a3b9c5eaf3fbe32
bebora <simone3.rizzo@mail.polimi.it>
2019-05-05 10:56:21 +0200

    Remove unused panels and add destination form in singleroute

--- scripts/singleroute.js
+++ scripts/singleroute.js
@@ -22,35 +22,6 @@ var sidebar = L.control.sidebar({ container: 'sidebar' })
 .addTo(map)
 .open('home');
 
-// add panels dynamically to the sidebar
-sidebar
-.addPanel({
-    id:   'js-api',
-    tab:  '<i class="fa fa-cog"></i>',
-    title: 'JS API',
-    pane: '<p>The Javascript API allows to dynamically create or modify the panel state.<p/><p><button onclick="sidebar.enablePanel(\'mail\')">enable mails panel</button><button onclick="sidebar.disablePanel(\'mail\')">disable mails panel</button></p><p><button onclick="addUser()">add user</button></b>',
-})
-// add a tab with a click callback, initially disabled
-.addPanel({
-    id:   'mail',
-    tab:  '<i class="fa fa-envelope"></i>',
-    title: 'Messages',
-    button: function() { alert('opened via JS callback') },
-    disabled: true,
-})
-
-// be notified when a panel is opened
-sidebar.on('content', function (ev) {
-    switch (ev.id) {
-        case 'autopan':
-        sidebar.options.autopan = true;
-        break;
-        default:
-        sidebar.options.autopan = false;
-    }
-});
-
-
 
 var start_lat;
 var start_lng;
@@ -169,8 +140,8 @@ map.on("click", function(e){
 });
 
 
-var engine = new PhotonAddressEngine();
-$("#text-search1").typeahead(null, {
+var engine = new PhotonAddressEngine({'lang': 'it'});
+$(".photon-search").typeahead(null, {
     source: engine.ttAdapter(),
     displayKey: 'description'
 });
